da945e7cd9bcb6b5533bc7bb94297b1aa44e770b
apetrenko <apetrenko@gseis.ru>
2026-08-31 21:48:48 +0500

    временно открыл доступ к БД.
    закомментировал некоторые строки.

--- TimeManagementService/Program.cs
+++ TimeManagementService/Program.cs
@@ -32,13 +32,13 @@ public class Program
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
+        // if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
         }
 
-        app.UseHttpsRedirection();
+        // app.UseHttpsRedirection();
 
         app.UseAuthorization();
 
